4f99cca0eaa2a79d351710f95e43d8a86465aaa8
jorge <jbousing@gmail.com>
2024-01-27 23:43:11 -0400

    funcionalidad de oceano terminada

--- Scripts/Ocean.gd
+++ Scripts/Ocean.gd
@@ -1,6 +1,10 @@
 extends KinematicBody2D
 
 onready var player = get_parent().get_node("Niguana M")
+onready var waterarea = get_node("waterarea")
 
 func _physics_process(delta):
-	self.position.x = player.position.x
+	#self.position.x = player.position.x
+	if player != null && player.position != null:
+		waterarea.position.x = player.position.x
+		self.position.x = player.position.x
